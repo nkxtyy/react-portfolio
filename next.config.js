b1d2e865c62dbfcfaa7163e5d73346833942bc06
Kriti <othersss2020@gmail.com>
2024-07-19 20:11:43 +0530

    Next commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Enable static export
+  output: 'export',
+  basePath: '/react-portfolio',
+  images: {
+    unoptimized: true,  // Disable image optimization
+  },
+  // Other Next.js configurations if needed
+};
+
+module.exports = nextConfig;
